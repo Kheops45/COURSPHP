aa70a778c9daaff06ec9d8ef2a07a44d7b321d45
Kheops45 <95616842+Kheops45@users.noreply.github.com>
2024-03-11 19:50:02 +0100

    Add files via upload

---
+++ jour2exorecap.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fiche de poste</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="bg-dark">
+
+<div class="container-fluid mt-2">
+
+<div class="alert alert-success text-center" role="alert">
+<h1>Fiche de poste Développeur Web</h1>
+</div>
+
+
+<div class="row mx-0 align-items-center">
+
+<div class="col-md-4  text-center">
+<!-- Colonne videos -->
+<iframe width="560" height="315" src="https://www.youtube.com/embed/AeW2Z69FRhs?si=OgTU_Sy8T_Fqa01B&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+<!-- Fin colonne video -->
+</div>
+
+
+<div class="col-md-8 mx-0">
+<!-- Colonne php de 8 -->
+<div class="alert alert-warning text-center" role="alert">
+<?php echo '<H1>Ici, le PHP est utilisé</h1>';?>
+</div>
+
+
+<div class="alert alert-primary text-center" role="alert">
+<?php echo '<h3>Intitulé du poste : Développeur WEB</h3>';?>
+</div>
+
+
+<div class="alert alert-secondary text-center" role="alert">
+<?php echo '<b>Technologies maîtrisées : </b>','<br> <br>', 'HTML','<br>';
+echo 'CSS','<br>';
+echo 'JavaScript','<br>';
+echo 'Bootstrap','<br>';
+echo 'PHP','<br>';
+echo 'Mysql','<br><br>';
+
+
+?>
+
+<div class="alert alert-danger text-center" role="alert">
+<?php echo 'Salaire Moyen d\'un Dev junior : 2 333,33 € * 12 Mois : ',2333.33*12,' Euros';?>
+</div>
+
+
+
+</div>
+
+
+
+</div><!-- Fin colonne  de 8 -->
+
+
+
+</div> <!-- Fin Ligne 1 -->
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</div><!-- Fin Container -->
+</body>
+</html>
